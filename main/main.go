f754c424cc508f1dc2e54563f2a4e8b5ba71ceb7
inipew <dhimas@akupew.toys>
2024-08-13 09:03:51 +0700

    initial

---
+++ main/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"wth/main/caddy"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		printUsage()
+		return
+	}
+
+	command := os.Args[1]
+	switch command {
+	case "caddy":
+		if len(os.Args) < 3 {
+			printCaddyUsage()
+			return
+		}
+
+		subcommand := os.Args[2]
+		switch subcommand {
+		case "install":
+			caddy.InstallCaddy()
+		case "start":
+			caddy.StartCaddy()
+		case "restart":
+			caddy.RestartCaddy()
+		case "stop":
+			caddy.StopCaddy()
+		case "changedomain":
+			if len(os.Args) < 4 {
+				fmt.Println("Usage: ./binary caddy changedomain <newdomain>")
+				return
+			}
+			newDomain := os.Args[3]
+			caddy.ChangeDomain(newDomain)
+		default:
+			fmt.Println("Unknown subcommand:", subcommand)
+		}
+	default:
+		fmt.Println("Unknown command:", command)
+	}
+}
+
+func printUsage() {
+	fmt.Println("Usage: ./binary <command> [options]")
+	fmt.Println("Commands:")
+	fmt.Println("  caddy <subcommand> - Manage Caddy")
+}
+
+func printCaddyUsage() {
+	fmt.Println("Usage: ./binary caddy <subcommand> [options]")
+	fmt.Println("Subcommands:")
+	fmt.Println("  install       - Install Caddy")
+	fmt.Println("  start         - Start Caddy")
+	fmt.Println("  restart       - Restart Caddy")
+	fmt.Println("  stop          - Stop Caddy")
+	fmt.Println("  changedomain  - Change domain in Caddyfile")
+}
